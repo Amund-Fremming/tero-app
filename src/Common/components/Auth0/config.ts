ccf89130a32457b001d1279a3525157c12e355f1
Amund Fremming <amund.fremming@gmail.com>
2025-10-06 17:13:45 +0200

    Updated lpogin values

--- src/Common/components/Auth0/config.ts
+++ src/Common/components/Auth0/config.ts
@@ -1,7 +1,7 @@
 import * as AuthSession from "expo-auth-session";
 
-const CLIENT_ID = "8XF85icFfdDejkrewfsFtOg5kfj6LqG8";
-const DOMAIN = "dev-8gif55bqfum82z1v.us.auth0.com";
+const CLIENT_ID = "vcrgS1imJrANviUILgZ7xlq6YZFbPnQD";
+const DOMAIN = "dev-tero.eu.auth0.com";
 const SCHEME = "com.tero";
 const AUDIENCE = "https://api.tero.com";
 
@@ -20,15 +20,15 @@ interface IDiscovery {
 }
 
 const discovery: IDiscovery = {
-  authorizationEndpoint: `https://${DOMAIN}/authorize`,
-  tokenEndpoint: `https://${DOMAIN}/oauth/token`,
-  revocationEndpoint: `https://${DOMAIN}/oauth/revoke`,
+    authorizationEndpoint: `https://${DOMAIN}/authorize`,
+    tokenEndpoint: `https://${DOMAIN}/oauth/token`,
+    revocationEndpoint: `https://${DOMAIN}/oauth/revoke`,
 };
 
 export const Auth0Config: IAuth0Config = {
     domain: DOMAIN,
     clientId: CLIENT_ID,
     audience: AUDIENCE,
-    redirectUri: AuthSession.makeRedirectUri({ scheme: SCHEME}),
+    redirectUri: AuthSession.makeRedirectUri({ scheme: SCHEME }),
     discovery
 }
